5b2148ab0e9080f05f6aac99763c0902df5ac8ac
ovation22 <john@1north.com>
2022-12-12 10:54:24 -0500

    Serilog

--- src/Api.Project.Template.Api/Program.cs
+++ src/Api.Project.Template.Api/Program.cs
@@ -3,6 +3,7 @@ using Api.Project.Template.Infrastructure.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Api.Project.Template.Api;
 
@@ -11,6 +12,9 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.UseSerilog((ctx, lc) =>
+            lc.ReadFrom.Configuration(ctx.Configuration));
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
