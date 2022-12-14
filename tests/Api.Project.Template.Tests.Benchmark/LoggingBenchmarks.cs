ef03ecf24d3ea37ad250b2c01d82f11d99a2425f
ovation22 <john@1north.com>
2022-12-13 21:08:06 -0500

    Added Benchmark Tests

---
+++ tests/Api.Project.Template.Tests.Benchmark/LoggingBenchmarks.cs
@@ -0,0 +1,46 @@
+﻿using Api.Project.Template.Core.Interfaces.Logging;
+using Api.Project.Template.Infrastructure.Logging;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Project.Template.Tests.Benchmark;
+
+[MemoryDiagnoser]
+public class LoggingBenchmarks
+{
+    private readonly ILogger<LoggingBenchmarks> _logger;
+    private readonly ILoggerAdapter<LoggingBenchmarks> _loggerAdapter;
+
+    public LoggingBenchmarks()
+    {
+        ILoggerFactory? loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .SetMinimumLevel(LogLevel.Warning);
+        });
+
+        _logger = new Logger<LoggingBenchmarks>(loggerFactory);
+        _loggerAdapter = new LoggerAdapter<LoggingBenchmarks>(_logger);
+    }
+
+    [Benchmark]
+    public void Logger_Without_If()
+    {
+        _logger.LogInformation("test {0}", 42);
+    }
+
+    [Benchmark]
+    public void Logger_With_If()
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation("test {0}", 42);
+        }
+    }
+
+    [Benchmark]
+    public void LoggerAdapter()
+    {
+        _loggerAdapter.LogInformation("test {0}", 42);
+    }
+}
---
+++ tests/Api.Project.Template.Tests.Benchmark/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Api.Project.Template.Tests.Benchmark;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run(typeof(Program).Assembly);
+    }
+}
