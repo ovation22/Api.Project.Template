de3e5f08d2195d761d0dfdf4a8cd22b3ce2ebcc4
ovation22 <john@1north.com>
2022-12-21 15:50:12 -0500

    Added WeatherForcastService tests

---
+++ tests/Api.Project.Template.Tests.Unit/Core/Services/WeatherForecastService/GetWeatherForecastTests.cs
@@ -0,0 +1,25 @@
+﻿using Api.Project.Template.Core.Models.DTO;
+using Xunit;
+
+namespace Api.Project.Template.Tests.Unit.Core.Services.WeatherForecastService;
+
+public class GetWeatherForecastTests
+{
+    private readonly Template.Core.Services.WeatherForecastService _weatherForecastService;
+
+    public GetWeatherForecastTests()
+    {
+        _weatherForecastService = new Template.Core.Services.WeatherForecastService();
+    }
+
+    [Fact]
+    public void WhenCalled_ThenWeatherForecastReturned()
+    {
+        // Arrange
+        // Act
+        var result = _weatherForecastService.GetWeatherForecast();
+
+        // Assert
+        Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+    }
+}
