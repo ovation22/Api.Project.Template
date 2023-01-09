8597919bd5e45d70bf8a7b867b6daca6e5f72c36
ovation22 <john@1north.com>
2023-01-09 16:16:21 -0500

    NuGet update

--- src/Api.Project.Template.Api/Config/HealthConfig.cs
+++ src/Api.Project.Template.Api/Config/HealthConfig.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.IO;
-using System.Text.Json;
-using System.Text;
-using System.Threading.Tasks;
 using Api.Project.Template.Infrastructure.Data;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Api.Project.Template.Api.Config;
 
@@ -26,54 +21,12 @@ public static class HealthConfig
     {
         app.MapHealthChecks("/healthz/ready", new HealthCheckOptions
         {
-            ResponseWriter = WriteResponse
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
         });
 
         app.MapHealthChecks("/healthz/live", new HealthCheckOptions
         {
             Predicate = _ => false
         });
-    }
-
-    private static Task WriteResponse(HttpContext context, HealthReport healthReport)
-    {
-        context.Response.ContentType = "application/json; charset=utf-8";
-
-        var options = new JsonWriterOptions { Indented = true };
-
-        using var memoryStream = new MemoryStream();
-        using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
-        {
-            jsonWriter.WriteStartObject();
-            jsonWriter.WriteString("status", healthReport.Status.ToString());
-            jsonWriter.WriteStartObject("results");
-
-            foreach (var healthReportEntry in healthReport.Entries)
-            {
-                jsonWriter.WriteStartObject(healthReportEntry.Key);
-                jsonWriter.WriteString("status",
-                    healthReportEntry.Value.Status.ToString());
-                jsonWriter.WriteString("description",
-                    healthReportEntry.Value.Description);
-                jsonWriter.WriteStartObject("data");
-
-                foreach (var item in healthReportEntry.Value.Data)
-                {
-                    jsonWriter.WritePropertyName(item.Key);
-
-                    JsonSerializer.Serialize(jsonWriter, item.Value,
-                        item.Value.GetType());
-                }
-
-                jsonWriter.WriteEndObject();
-                jsonWriter.WriteEndObject();
-            }
-
-            jsonWriter.WriteEndObject();
-            jsonWriter.WriteEndObject();
-        }
-
-        return context.Response.WriteAsync(
-            Encoding.UTF8.GetString(memoryStream.ToArray()));
     }
 }
