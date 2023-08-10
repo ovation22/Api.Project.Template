ec49478aefb9ddab0e808718aa09ecf2842f07be
John Callaway <ovation22@users.noreply.github.com>
2023-08-10 14:47:21 -0400

    Dtos should not have a dependency on Entities (#6)

---
+++ tests/Api.Project.Template.Tests.Architecture/DtoTests.cs
@@ -0,0 +1,28 @@
+﻿using Api.Project.Template.Core.Models.DTO;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace Api.Project.Template.Tests.Architecture;
+
+public class DtoTests
+{
+    private readonly Types _types;
+
+    public DtoTests()
+    {
+        _types = Types.InAssembly(typeof(WeatherForecast).Assembly);
+    }
+
+    [Fact]
+    public void Dto_ShouldNotReference_Entities()
+    {
+        var result = _types
+            .That()
+            .ResideInNamespace("Api.Project.Template.Core.Models.DTO")
+            .ShouldNot()
+            .HaveDependencyOn("Api.Project.Template.Core.Models.Entities")
+            .GetResult();
+
+        Assert.True(result.IsSuccessful, result.GetFailingTypes());
+    }
+}
