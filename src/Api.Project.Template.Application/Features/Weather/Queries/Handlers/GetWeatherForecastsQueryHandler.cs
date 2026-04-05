f4c13124bf2aca7c2d1f7909015228d9b1f4d25f
John Callaway <john@1north.com>
2026-04-05 18:25:16 -0400

    Collapsed WeatherForecastService into the Handler.

--- src/Api.Project.Template.Application/Features/Weather/Queries/Handlers/GetWeatherForecastsQueryHandler.cs
+++ src/Api.Project.Template.Application/Features/Weather/Queries/Handlers/GetWeatherForecastsQueryHandler.cs
@@ -1,22 +1,18 @@
+using Api.Project.Template.Application.Abstractions;
 using Api.Project.Template.Application.Common.Pagination;
-using Api.Project.Template.Application.Features.Weather.Services;
+using Api.Project.Template.Application.Features.Weather.Specifications;
 using MediatR;
 
 namespace Api.Project.Template.Application.Features.Weather.Queries.Handlers;
 
-public class GetWeatherForecastsQueryHandler : IRequestHandler<GetWeatherForecastsQuery, PagedList<GetWeatherForecastsResponse>>
+public class GetWeatherForecastsQueryHandler(IRepository repository)
+    : IRequestHandler<GetWeatherForecastsQuery, PagedList<GetWeatherForecastsResponse>>
 {
-    private readonly IWeatherForecastService _weatherForecastService;
-
-    public GetWeatherForecastsQueryHandler(IWeatherForecastService weatherForecastService)
-    {
-        _weatherForecastService = weatherForecastService;
-    }
-
     public async Task<PagedList<GetWeatherForecastsResponse>> Handle(
         GetWeatherForecastsQuery request,
         CancellationToken cancellationToken)
     {
-        return await _weatherForecastService.GetForecastsAsync(request.PaginationRequest, cancellationToken);
+        var spec = new WeatherForecastFilterSpecification(request.PaginationRequest);
+        return await repository.ListAsync(spec, cancellationToken);
     }
 }
