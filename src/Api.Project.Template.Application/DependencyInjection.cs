11bbcc792dce64055faa4ef10616277b209f2c24
John Callaway <ovation22@users.noreply.github.com>
2026-04-05 18:45:57 -0400

    Collapsed WeatherForecastService into the Handler. (#22)

--- src/Api.Project.Template.Application/DependencyInjection.cs
+++ src/Api.Project.Template.Application/DependencyInjection.cs
@@ -1,4 +1,3 @@
-using Api.Project.Template.Application.Features.Weather.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.Project.Template.Application;
@@ -8,8 +7,6 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-
-        services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 
         return services;
     }
